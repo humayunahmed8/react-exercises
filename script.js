924eefb47b55a18b39d3c914b5b398ad8812d43c
humayunahmed8 <developer.humayun@gmail.com>
2024-07-02 15:41:02 +0600

    Print hello world in React

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-let number = 0;
-const button = document.querySelector("#button");
-const display = document.querySelector("#display");
+// let number = 0;
+// const button = document.querySelector("#button");
+// const display = document.querySelector("#display");
 
-button.addEventListener("click", () => {
-  number++;
-  display.textContent = number;
-})
+// button.addEventListener("click", () => {
+//   number++;
+//   display.textContent = number;
+// })
+
+const { createRoot } = ReactDOM;
+
+console.log(ReactDOM);
+
+const domContainer = document.querySelector("#root");
+const root = createRoot(domContainer);
+
+function App() {
+  return <h1>Hello world</h1>;
+}
+
+root.render(<App />);
